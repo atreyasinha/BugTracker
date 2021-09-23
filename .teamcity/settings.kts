e18149b9c78218e505ea4dd10d9dd275352cf4a4
Atreya <84874851+atreyasinha@users.noreply.github.com>
2021-09-22 17:24:18 -0700

    update build steps

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.script
 
 version = "2021.1"
 
@@ -16,7 +17,9 @@ object Build : BuildType({
     }
 
     steps {
-        
+        script {
+            scriptContent = "echo 'Hello world!'"
+        }
     }
 
     triggers {
