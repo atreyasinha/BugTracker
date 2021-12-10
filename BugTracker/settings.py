1869d12e4481c8ff3c9236e3b807825ab7c2d700
Atreya <84874851+atreyasinha@users.noreply.github.com>
2021-12-09 17:19:31 -0800

    add new path, gcp

--- BugTracker/settings.py
+++ BugTracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$)x91r6@nw(k(6ytf=bj$-znae2h%jnebn(55sss3e06*xv1ha
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'b-t-deploy-l2mcnty4ja-uw.a.run.app']
+ALLOWED_HOSTS = ['0.0.0.0', 'b-t-deploy-l2mcnty4ja-uw.a.run.app', '127.0.0.1', 'b-t-deploy-6z6ne2xu7q-uw.a.run.app']
 
 # Application definition
 
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'issue'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
