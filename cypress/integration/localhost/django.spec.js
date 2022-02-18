aa4580391ae24b865bd73fe679642224bb52e13b
Atreya <84874851+atreyasinha@users.noreply.github.com>
2022-02-18 11:35:09 -0800

    change gcp account, deployment

--- cypress/integration/localhost/django.spec.js
+++ cypress/integration/localhost/django.spec.js
@@ -1,9 +1,16 @@
 /// <reference types="cypress" />
 
 describe('django localhost page', () => {
-  it('displays default django server', () => {
+  // it('displays default django server', () => {
+  //   cy.visit('/')
+  //   cy.get('.logo').should('have.text', '\n            django\n          ')
+  //   cy.contains('The install worked successfully! Congratulations!')
+  // })
+
+  it('displays hero text', () => {
     cy.visit('/')
-    cy.get('.logo').should('have.text', '\n            django\n          ')
-    cy.contains('The install worked successfully! Congratulations!')
+    cy.contains('Big ideas come with small bugs')
+    cy.contains('Buggerfly is a fundamentally oriented investment group that identifies and captures compelling long-term investment opportunities presented by market inefficiencies')
+    cy.contains('Affordable Pricing')
   })
 })
