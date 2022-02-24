5d2079ced34af5a71d98591b7352e5f7f3bcbbfe
Atreya <84874851+atreyasinha@users.noreply.github.com>
2022-02-24 14:12:42 -0800

    update, django server

--- cypress/integration/localhost/django.spec.js
+++ cypress/integration/localhost/django.spec.js
@@ -1,16 +1,16 @@
 /// <reference types="cypress" />
 
 describe('django localhost page', () => {
-//   it('displays default django server', () => {
-//     cy.visit('/')
-//     cy.get('.logo').should('have.text', '\n            django\n          ')
-//     cy.contains('The install worked successfully! Congratulations!')
-//   })
-
-  it('displays hero text', () => {
+  it('displays default django server', () => {
     cy.visit('/')
-    cy.contains('Big ideas come with small bugs')
-    cy.contains('Buggerfly is a fundamentally oriented investment group that identifies and captures compelling long-term investment opportunities presented by market inefficiencies')
-    cy.contains('Affordable Pricing')
+    cy.get('.logo').should('have.text', '\n            django\n          ')
+    cy.contains('The install worked successfully! Congratulations!')
   })
+
+  // it('displays hero text', () => {
+  //   cy.visit('/')
+  //   cy.contains('Big ideas come with small bugs')
+  //   cy.contains('Buggerfly is a fundamentally oriented investment group that identifies and captures compelling long-term investment opportunities presented by market inefficiencies')
+  //   cy.contains('Affordable Pricing')
+  // })
 })
