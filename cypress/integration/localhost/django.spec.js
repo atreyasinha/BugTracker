3d7354ffa71834290f57500f2d73dccc47f713a9
Atreya <84874851+atreyasinha@users.noreply.github.com>
2022-02-28 09:54:53 -0800

    update:Buggerfly

--- cypress/integration/localhost/django.spec.js
+++ cypress/integration/localhost/django.spec.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress" />
 
 describe('django localhost page', () => {
-  it('displays default django server', () => {
-    cy.visit('/')
-    cy.get('.logo').should('have.text', '\n            django\n          ')
-    cy.contains('The install worked successfully! Congratulations!')
-  })
+  // it('displays default django server', () => {
+  //   cy.visit('/')
+  //   cy.get('.logo').should('have.text', '\n            django\n          ')
+  //   cy.contains('The install worked successfully! Congratulations!')
+  // })
 
   it('displays hero text', () => {
     cy.visit('/')
